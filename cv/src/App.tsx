4b427e8d4ee12d6c9fcecc5e6d3adb07c9fe684e
N Samijo <nsamijo@gmail.com>
2026-09-08 13:05:30 +0200

    Update frontpage

--- cv/src/App.tsx
+++ cv/src/App.tsx
@@ -8,16 +8,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Check back soon for a new and improved version of my CV!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
